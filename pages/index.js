2b0ad8d948d456af807654fe44de2a68b2df479f
Bradley Burgess <bradleyburgess@gmail.com>
2021-07-27 16:54:16 -0400

    Front page Gathering

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,27 @@ import { theme } from "../styles/global";
 import Head from "next/head";
 import Image from "next/image";
 import roseWindow from "../public/rosette.png";
+import gatheringLogo from "../public/gatheringlogo_mark.png";
 import Button from "../components/common/Button";
 import ESUMCLogo from "../components/common/ESUMCLogo";
+import { makeTitleCase } from "../lib/stringUtils";
+
+const locations = [
+  {
+    image: {
+      src: roseWindow,
+      alt: "Rose Window",
+    },
+    name: "sanctuary",
+  },
+  {
+    image: {
+      src: gatheringLogo,
+      alt: "Gathering Logo",
+    },
+    name: "gathering",
+  },
+];
 
 export default function Home() {
   return (
@@ -18,20 +37,22 @@ export default function Home() {
             <h1 style={{ marginTop: "1rem" }}>ESUMC Online Bulletins</h1>
           </header>
           <main>
-            <section style={{ marginTop: "2rem" }}>
-              <div className="sectionHeader">
-                <div className="sectionLogo">
-                  <Image src={roseWindow} alt="Rose Window" />
+            {locations.map((location) => (
+              <section style={{ marginTop: "2rem" }} key={location.name}>
+                <div className="sectionHeader">
+                  <div className="sectionLogo">
+                    <Image src={location.image.src} alt={location.image.alt} />
+                  </div>
+                  <h2>{makeTitleCase(location.name)}</h2>
                 </div>
-                <h2>Sanctuary</h2>
-              </div>
-              <div className="buttons">
-                <Button href="/sanctuary/latest" primary>
-                  Latest
-                </Button>
-                <Button href="/sanctuary">Archive</Button>
-              </div>
-            </section>
+                <div className="buttons">
+                  <Button href={`/${location.name}/latest`} primary>
+                    Latest
+                  </Button>
+                  <Button href={`/${location.name}`}>Archive</Button>
+                </div>
+              </section>
+            ))}
           </main>
         </div>
       </div>
