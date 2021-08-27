43c0c6b668428cb6e482dd10e49129ebf61ea187
azalus88 <bradleyburgess@gmail.com>
2021-08-27 13:10:52 -0400

    Revert to italics

--- components/common/Copyright/index.js
+++ components/common/Copyright/index.js
@@ -1,16 +1,17 @@
 const Copyright = ({ children }) => (
   <>
-    <footer className="root">
+    <footer className="copyright">
       <p className="text">{children}</p>
     </footer>
     <style jsx>{`
-      .root {
+      .copyright {
         display: flex;
         justify-content: center;
         margin-top: 2rem;
       }
       .text {
         max-width: 400px;
+        font-style: italic;
         text-align: center;
         line-height: 1.15em;
       }
